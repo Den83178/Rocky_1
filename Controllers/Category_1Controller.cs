5c8da00ae64d2e989c74dfc6e483583215dac354
denism <den83178@gmail.com>
2024-08-26 19:49:56 +0300

    027.Added constructor Category_1(DI container)

--- Controllers/Category_1Controller.cs
+++ Controllers/Category_1Controller.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Rocky_1.Models;
+using Rocky_1.Name;
 
 namespace Rocky_1.Controllers
 {
     public class Category_1Controller : Controller
     {
+        private readonly ApplicationDbContext_1 _db1;
+
+        public Category_1Controller(ApplicationDbContext_1 db)
+        {
+            _db1 = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category_1 > objList = _db1.Category_1;
+            return View(objList);
         }
     }
 }
